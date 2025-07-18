8830d388cc76e86871ca53b96703dc0f4d99be79
x-kevinbro <damiruxox@outlook.com>
2025-07-18 11:15:02 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -102,12 +102,11 @@ async def start_me(bot, update):
                         InlineKeyboardButton("Help", callback_data = "ghelp")
                 ],
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Support Channel', url='https://t.me/+4M1goFtfP2Y3ZDM1'),
+                    InlineKeyboardButton('Feedback', url='https://t.me/DirectLink_Free')
                 ],
                 [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Other Bots', url='https://t.me/c/2804547624/4')
                 ]
             ]
         ),
